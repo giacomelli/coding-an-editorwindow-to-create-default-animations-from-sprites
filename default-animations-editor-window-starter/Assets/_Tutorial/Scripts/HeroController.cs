d4a33a472c0d3745c06bc828b7bda1c396830310
giacomelli <giacomelli@gmail.com>
2019-05-26 13:27:22 -0300

    Starter project

---
+++ default-animations-editor-window-starter/Assets/_Tutorial/Scripts/HeroController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class HeroController : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+    Animator _anim;
+
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        _renderer.flipX = h < 0;
+        _anim.SetFloat("DirectionX", h);
+        _anim.SetFloat("DirectionY", v);
+        _anim.SetBool("Walk", !Mathf.Approximately(h, 0) || !Mathf.Approximately(v, 0));
+    }
+}
